a4d826f5d2e01d9f78b35f85f97ea9d7b9ff2c36
Paul Pedro <unkerpaulie@gmail.com>
2022-10-08 06:42:26 -0400

    added functions from database to main

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from model import Todo
+
+import database as db
 
 # app object
 app = FastAPI()
@@ -21,30 +24,44 @@ def read_root():
 
 @app.get("api/todo")
 async def get_todo():
-    return 1
-
-@app.get("api/todo")
-async def get_todo():
     """Get all todo items"""
-    return 1
+    response = await db.fetch_all_items()
+    return response
 
-@app.get("api/todo{id}")
-async def get_todo_by_id(id):
-    """Get a todo item by id"""
-    return 1
+@app.get("api/todo{title}", response_model=Todo)
+async def get_todo_by_title(title):
+    """Get a todo item by title"""
+    response = await db.fetch_one_item(title)
+    if response:
+        return response
+    raise HTTPException(404, f"The item: \"{title}\" was not found.")
 
-@app.post("api/todo/new")
-async def post_todo(data):
+@app.post("api/todo/new", response_model=Todo)
+async def new_todo(data):
     """Create a new todo item"""
-    return 1
-
-@app.put("api/todo/edit{id}")
-async def edit_todo(id, data):
+    response = await db.create_item(data.dict)
+    if response:
+        return response
+    raise HTTPException(400, f"Something went wrong.")
+    
+@app.put("api/todo/edit{title}")
+async def edit_todo(title, data):
     """Edit (update) a todo item"""
     return 1
 
-@app.delete("api/todo/delete{id}")
-async def delete_todo(id):
-    """Edit (update) a todo item"""
-    return 1
+@app.put("api/todo/check{title}")
+async def check_todo(title):
+    """Toggle a todo item's done status"""
+    response = await db.check_item(title)
+    if response:
+        return response
+    raise HTTPException(404, f"The item: \"{title}\" was not found.")
+
+@app.delete("api/todo/delete{title}")
+async def delete_todo(title):
+    """Delete a todo item"""
+    response = await db.delete_item(title)
+    if response:
+        return {"msg": f"Successfully deleted item: \"{title}\""}
+    raise HTTPException(404, f"The item: \"{title}\" was not found.")
 
