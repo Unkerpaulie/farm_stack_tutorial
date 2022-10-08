f72057d4577768c469120a25b1622e686e20e13e
Paul Pedro <unkerpaulie@gmail.com>
2022-10-08 05:32:56 -0400

    added methods and routes for all crud operations

--- backend/main.py
+++ backend/main.py
@@ -19,3 +19,32 @@ app.add_middleware(
 def read_root():
     return {"msg": "Hello!"}
 
+@app.get("api/todo")
+async def get_todo():
+    return 1
+
+@app.get("api/todo")
+async def get_todo():
+    """Get all todo items"""
+    return 1
+
+@app.get("api/todo{id}")
+async def get_todo_by_id(id):
+    """Get a todo item by id"""
+    return 1
+
+@app.post("api/todo/new")
+async def post_todo(data):
+    """Create a new todo item"""
+    return 1
+
+@app.put("api/todo/edit{id}")
+async def edit_todo(id, data):
+    """Edit (update) a todo item"""
+    return 1
+
+@app.delete("api/todo/delete{id}")
+async def delete_todo(id):
+    """Edit (update) a todo item"""
+    return 1
+
